879339a8ec1d3488547c4bb442d2492571718a35
ramen6315 <ramen6315@gmail.com>
2021-07-27 14:45:36 +0900

    feat : description PhoneKeMone solution

--- java/src/main/java/leejunghyeok/PhoneKeMon.java
+++ java/src/main/java/leejunghyeok/PhoneKeMon.java
@@ -18,6 +18,8 @@ public class PhoneKeMon {
         for (int num : nums) {
             phoneStyle.add(num);
         }
+        // 최대 뽑을 수 있는 포켓몬과 실제로 뽑을 수 있는 포켓몬수 중 작은 값을 출력
+        // 3번째 case를 보면 최대 뽑을 수 있는 포켓몬 수는 3마리지만 실제로 뽑을 수 있는 폰켓몬 의 수는 2마리 (3,2)
         return Math.min(phoneStyle.size(), nums.length / 2);
     }
 }
