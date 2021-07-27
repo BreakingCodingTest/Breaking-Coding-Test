6636ccb5fc5e68e988d391fcf427bbcae63f094d
ramen6315 <ramen6315@gmail.com>
2021-07-27 19:08:35 +0900

    feat : solution description

--- java/src/main/java/leejunghyeok/CraneGame.java
+++ java/src/main/java/leejunghyeok/CraneGame.java
@@ -3,6 +3,11 @@ package leejunghyeok;
 import java.util.Stack;
 
 /**
+ * 처음에 작성한 코드 stack의 크기에 따라 로직을 다르게 함
+ * 뽑힌 인형을 저장하는 공간인 stack의 크기가 0일경우 값을 넣고 뽑힌 인형의 위치 값을 0으로 변경
+ * 0이 아닌경우 맨위에 있는 것을 pick 변수에 넣고 뽑힌 인형의 위치를 0으로 변경
+ * 그 다음에 pick의 값에 따라서 stack의 맨위 값과 비교하여 +2 또는 그냥 넘어감
+ *
  * int cnt = 0;
  *         Stack<Integer> stack = new Stack<>();
  *         for (int move : moves) {
@@ -41,15 +46,20 @@ public class CraneGame {
     }
     public static int solution(int[][] board, int[] moves) {
         int cnt = 0;
+        //걸린 인형들을 저장하기 위한 공간
         Stack<Integer> stack = new Stack<>();
+
+        //위치 배열을 반복하면서 가장위에 있는 인형을 찾기 위한 2중 반복문
         for (int move : moves) {
             for (int j = 0; j < board.length; j++) {
+                // 맨위에 있는 인형일경우 stack에 저장 후 해당 인형 위치 0 으로 변경
                 if (board[j][move - 1] != 0) {
                     stack.push(board[j][move - 1]);
                     board[j][move - 1] = 0;
                     break;
                 }
             }
+            //크기가 2이상 인지 확인하고 맞으면 맨위 2개를 꺼내서 확인 맞으면 +2점 아니면 다시 넣는다.
             if(stack.size() > 1) {
                 int top = stack.pop();
                 int secondTop = stack.pop();
