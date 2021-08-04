6efe4c1c74e905c7cb36c4543f15792486e31fdd
hcs4125 <64846408+hcs4125@users.noreply.github.com>
2021-08-04 16:59:22 +0900

    Update CrainGame.java

--- changsub/week1/CrainGame.java
+++ changsub/week1/CrainGame.java
@@ -3,19 +3,17 @@ import java.util.Stack;
 public class CrainGame {
     public static int solution(int[][] board, int[] moves) {
         int answer = 0;
-        Stack<Integer> res_stack = new Stack<>();//결과를 반영할 스택
-        /*
-            n*n 매트릭스에서 세로열의 연속적인 두 원소 중,
-            작은 인데스가 0이고 큰 인덱스가 0이 아닌 원소가
-            해당 세로열 중에서 가장 상위에 위치한 원소이다.
-            즉, 인형뽑기 시 해당열에서 뽑아져 올라오는 원소이다.
-        */
+        Stack<Integer> res_stack = new Stack<>();//결과를 반영할 스택, 인형을 뽑은 후 인형을 쌓는 스택이라고 이해하기 
         int peek_value = -1;
         int count = 0;
         int row = 0;
         int col = 0;
         boolean checkFlag = true; //반복문에서 세로열로 한번만 접근했는지 체크
-        //위에까지 인형이 차 있는경우 구현하기
+        /*
+            move를 증가 시켜가면서 몇번째 열로 접근 할지를 결정한다.
+            Line 18 : 만약 보드의 숫자가 0이 아니라면, 즉 숫자가 들어가 있다 == 인형이 존재하면,
+            res_stack에 해당 값을 푸쉬하고 해당 스택을 peek()해서 연속으로 같은수가 두번 들어가는 체크한다.
+        */
         for (int move : moves) {
             for (int j = 0; j < board.length; j++) {
                 if(board[j][move - 1] != 0){
@@ -46,10 +44,6 @@ public class CrainGame {
     }
 
     public static void main(String[] args) {
-        //System.out.println(solution(new int [][]{{2,0,1},{1,0,1},{3,3,2}},new int [] {2,1,3,3}));
-        //System.out.println(solution(new int [][]{{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}},new int [] {1,5,3,5,1,2,1,4}));
         System.out.println(solution(new int [][]{{0,0,1,0,0},{0,0,1,0,0},{0,2,1,0,0},{0,2,1,0,0},{0,2,1,0,0}},new int [] {1,2,3,3,2,3,1}));
-
-
     }
 }
