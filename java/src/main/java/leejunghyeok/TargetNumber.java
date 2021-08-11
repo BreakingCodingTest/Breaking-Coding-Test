3d63bf0526e11781bf24e4a7ef23f2c3b1c69de8
ramen6315 <ramen6315@gmail.com>
2021-08-11 16:38:17 +0900

    feat : 3rd week solution (target Number)

--- java/src/main/java/leejunghyeok/TargetNumber.java
+++ java/src/main/java/leejunghyeok/TargetNumber.java
@@ -9,6 +9,29 @@ public class TargetNumber {
     }
 
     public static int solution(int[] numbers, int target) {
-        return 0;
+        int index = 0;
+        int sum = 0;
+        int cnt = 0;
+
+        return depthR(numbers, target, index, sum, cnt);
+    }
+
+    public static int depthR(int[] numbers, int target, int index, int sum, int cnt) {
+        if(index == numbers.length) {
+            if(sum == target) {
+                return ++cnt;
+            } else {
+                return cnt;
+            }
+        }
+
+        int plusSum = sum + numbers[index];
+        int minusSum = sum - numbers[index];
+        index++;
+
+
+        cnt = depthR(numbers, target, index, plusSum, cnt);
+        cnt = depthR(numbers, target, index, minusSum, cnt);
+        return cnt;
     }
 }
